a4f89d85a506b4f0f8634ac95cca52c4ccec6a86
zzzaaaiiinnn <zain@chainelevate.com>
2026-02-09 16:42:07 +0400

    Update base path for github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // Force it to use the GitHub URL
-  site: 'https://butterflyio.github.io', 
-  base: '/Landing-Page_VARA', 
+  site: 'https://butterflyio.github.io',
+  base: '/Landing-Page_VARA',
   vite: {
     plugins: [tailwindcss()]
   }
