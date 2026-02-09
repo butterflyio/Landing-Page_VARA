19fc9a0590fe2b327a0031a1608cb6286908105d
zzzaaaiiinnn <zain@chainelevate.com>
2026-02-09 16:36:18 +0400

    Force reset site URL to github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  // Force it to use the GitHub URL
+  site: 'https://butterflyio.github.io', 
+  base: '/Landing-Page_VARA', 
   vite: {
     plugins: [tailwindcss()]
   }
